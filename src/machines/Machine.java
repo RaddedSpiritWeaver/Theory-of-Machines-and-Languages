81e2c799c0e0f48b7f00a38bab8e4bd54391ddb4
Erfan Noohi <erfan.noohi@gmail.com>
2018-03-11 15:05:42 +0330

    well now the boolean version working :) just need to clean the code

--- src/machines/Machine.java
+++ src/machines/Machine.java
@@ -7,13 +7,15 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Vector;
 
-// todo use a flagging mechanism to state acceptance
-// todo better reporting mechanisms and a better process block
+// todo use a flagging mechanism to state acceptance.
+// todo add a file writing system.
+// todo better reporting mechanisms and a better process block.
 public class Machine {
 
     private TransitionTable table = null;
     private StaticFileHolder fileHolder = null;
-    private String currentProcessingLine = "";
+    private String currentProcessingLine = ""; //todo try to make this not a class filed but something that is passed between calls
+
 
     public Machine(){
         this.fileHolder = new StaticFileHolder();
@@ -21,34 +23,19 @@ public class Machine {
     }
 
 
-    public void process(){
+    public void process(int s){
         for(int j = 0; j < 5; j++){
+            System.out.println(ConsoleColors.GREEN_BOLD_BRIGHT + "Machine Number:" + (j + 1) + ConsoleColors.RESET);
             try {
-                this.table.load(fileHolder.accessTo(0,j));
-                BufferedReader reader = new BufferedReader(new FileReader(fileHolder.accessTo(1, j)));
+                this.table.load(fileHolder.accessTo(2 * s,j));
+                BufferedReader reader = new BufferedReader(new FileReader(fileHolder.accessTo(2 * s + 1, j)));
                 currentProcessingLine = reader.readLine();
                 while (currentProcessingLine != null){
-                    System.out.println("Processsing !!!!:::\t" + currentProcessingLine);
+//                    System.out.println("Processing:\t" + currentProcessingLine);
                     int initialState = convertToInt(this.table.getInitalStage());
                     String trace = "";
-                    trasnsiton(initialState, 0, trace);
-                    currentProcessingLine = reader.readLine();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        for(int j = 0; j < 5; j++){
-            try {
-                this.table.load(fileHolder.accessTo(2,j));
-                BufferedReader reader = new BufferedReader(new FileReader(fileHolder.accessTo(3, j)));
-                currentProcessingLine = reader.readLine();
-                while (currentProcessingLine != null){
-                    System.out.println("Processsing !!!!:::\t" + currentProcessingLine);
-                    int initialState = convertToInt(this.table.getInitalStage());
-                    String trace = "";
-                    trasnsiton(initialState, 0, trace);
+                    boolean acceptance = transition(initialState, 0, trace);
+                    checkIfAccepted(acceptance, currentProcessingLine);
                     currentProcessingLine = reader.readLine();
                 }
             } catch (IOException e) {
@@ -57,26 +44,19 @@ public class Machine {
         }
     }
 
-    private void trasnsiton(int currentState, int index, String trace){ //todo a transiton function that returns a boolean and we check it with that
+    private boolean transition(int currentState, int index, String trace){ //todo a transition function that returns a boolean and we check it with that
         if(index  == currentProcessingLine.length()){ // reached the end of the string
-//            System.out.println("UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU");
-            System.out.println(ConsoleColors.BLUE_BOLD_BRIGHT + "current statet : " + ConsoleColors.RESET + currentState); // this confirms that current state is true
+//            System.out.println(ConsoleColors.BLUE_BOLD_BRIGHT + "current statet : " + ConsoleColors.RESET + currentState); // this confirms that current state is true
             String currentStateString = "" + (char)(currentState + 97);
             String[] finals = this.table.getFinalStages();
-            boolean isFinal = false;
             for(String s : finals){
-                System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "in the for with the string: " + ConsoleColors.RESET + s + " current state string \t" + currentStateString);
+//                System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "in the for with the string: " + ConsoleColors.RESET + s + " current state string \t" + currentStateString);
                 if(s.equals(currentStateString)){
-                    System.out.println("YES to " + currentProcessingLine);
-                    isFinal = true;
-                    break;
+//                    System.out.println("YES to " + currentProcessingLine);
+                    return true;
                 }
             }
-            if(!isFinal){
-                System.out.println("NO to" + currentProcessingLine);
-                System.out.println(ConsoleColors.GREEN_BOLD_BRIGHT + "with this trance : : \t" + ConsoleColors.RESET + trace);
-            }
-            return;
+            return false;
         }
 //        System.out.println("INDEEEXX: " + index + "\t\tline len :" + currentProcessingLine.length());
         int currentInput = Integer.parseInt(currentProcessingLine.substring(index, index + 1));
@@ -86,29 +66,36 @@ public class Machine {
         if(availableTransitions.isEmpty()){ // if we have no transitions we return
 //            System.out.println("broke at: " + (char)(currentState + 97)+ " with this input: " + currentInput + " with this trace:");
 //            System.out.println(trace);
-            return;
+            return false;
         }
         else{
-            for(String s : availableTransitions){
+            for(int i = 0; i < availableTransitions.size(); i++){
+                String s = availableTransitions.elementAt(i);
                 int nextState = convertToInt(s);
-                trasnsiton(nextState, index + 1, trace);
+                boolean transitionWorked = transition(nextState, index + 1, trace);
+                if(transitionWorked)
+                    return true;
             }
         }
-        if(index < currentProcessingLine.length()){
-//            System.out.println("NO to" + currentProcessingLine);
-        }
+        return false;
+    }
+
+    private void checkIfAccepted(boolean acceptance, String line){
+        if(acceptance)
+            System.out.println(ConsoleColors.BLUE_BOLD_BRIGHT + "ACCEPTED : " + ConsoleColors.RESET + line);
+        else
+            System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "NOT ACCEPTED : " + ConsoleColors.RESET + line);
     }
 
     private int convertToInt(String s){
-        s = s.toLowerCase(); // just to confirm every thing si in lower case :)
+        s = s.toLowerCase(); // just to confirm every thing is in lower case :)
         return s.charAt(0) - 97;
     }
 
     public static void main(String[] args) {
-//        String s = "012345";
-//        System.out.println(s.length());
-//        System.out.println(s.substring(5, 6));
-        new Machine().process();
+        new Machine().process(0);
+        System.out.println(ConsoleColors.PURPLE_BACKGROUND +  "\n\n" + ConsoleColors.RESET);
+        new Machine().process(1);
     }
 
 
